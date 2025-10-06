dc2a3a490d14aec975674e74168678c050db6bb8
alakhpandya <79519053+alakhpandya@users.noreply.github.com>
2025-10-06 09:58:30 +0530

    oct6

---
+++ 2025_Dhwanit_One_to_one/AS1.py
@@ -0,0 +1,12 @@
+import numpy as np 
+def get_array_properties(arr): 
+    ''' INPUT: arr -> A nD array OUTPUT: result -> tuple consisting of shape and dimension ''' 
+    ## STEP 1. Get the shape of array 
+    
+    
+    ## STEP 2. Get the dimension of array 
+    
+    
+    return shape,dim
+
+get_array_properties(np.array([0, 1, 2, 3, 4, 5]))
