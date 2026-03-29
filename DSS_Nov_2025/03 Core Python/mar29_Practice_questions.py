95ed477627e7f68a46c36df431ca8e3bb6e94c0f
alakhpandya <79519053+alakhpandya@users.noreply.github.com>
2026-03-29 12:16:04 +0530

    Create mar29_Practice_questions.py

---
+++ DSS_Nov_2025/03 Core Python/mar29_Practice_questions.py
@@ -0,0 +1,70 @@
+# Q-1
+"""
+A = input()
+print("Hello", A)
+"""
+# Q-2
+"""
+print("A")
+print("B")
+print("C")
+print("D")
+print("E")
+"""
+
+# Q-3:
+"""
+a = int(input())
+b = int(input())
+
+print(str(a) + str(b))
+"""
+
+# Q-4:
+"""
+a = input()
+b = input()
+print(a, "says hello to", b)
+print(f"{a} says hello to {b}")
+"""
+
+# Additional Problems
+# Q-1:
+"""
+t = int(input())
+print(40 - t)
+"""
+
+# Q-3:
+"""
+layers = int(input("Layers: "))
+prep_time = layers * 2
+baking_time = int(input("Baking time: "))
+print(prep_time+baking_time)
+"""
+
+# Q-6:
+"""
+x1 = int(input("x1: "))
+y1 = int(input("y1: "))
+x2 = int(input("x2: "))
+y2 = int(input("y2: "))
+
+slope = (y2 - y1)/(x2 - x1)
+print("Slope =", int(slope))
+"""
+# Q-8:
+"""
+day = input("Day: ")
+month = input("Month: ")
+year = input("Year: ")
+
+print(f"Date: {day}/{month}/{year}")
+"""
+
+# Q-9:
+a = float(input("a: "))
+b = float(input("b: "))
+c = float(input("c: "))
+
+print(a + b + c == 1)
