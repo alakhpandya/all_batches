953447dd6534ff9786094ea922bef82b6305c0f2
alakhpandya <79519053+alakhpandya@users.noreply.github.com>
2026-06-11 08:20:29 +0530

    Update first_llm_call.py

--- 2026_Tanish_Vihang_GenAI/03_Multi Agent/ai-basics/first_llm_call.py
+++ 2026_Tanish_Vihang_GenAI/03_Multi Agent/ai-basics/first_llm_call.py
@@ -6,8 +6,8 @@ load_dotenv()
 
 client = OpenAI(
     base_url="https://openrouter.ai/api/v1",
-    # api_key=os.getenv("OPENROUTER_API_KEY")
-    api_key="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"
+    api_key=os.getenv("OPENROUTER_API_KEY")
+    # api_key="your_api_key_here"
 )
 
 response = client.chat.completions.create(
