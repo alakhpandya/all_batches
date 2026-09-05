ce1cf3cc4dab4332f281318139af68828d3c0688
alakhpandya <79519053+alakhpandya@users.noreply.github.com>
2026-09-05 12:58:22 +0530

    sep5_2

--- DSS_Nov_2025/04 GenAI/03_MCP_Architecture/04_SQL_MCP_Server/client.py
+++ DSS_Nov_2025/04 GenAI/03_MCP_Architecture/04_SQL_MCP_Server/client.py
@@ -30,9 +30,140 @@ client = OpenAI(
 MODEL = os.getenv("MODEL_NAME")    # model name var from my .env file
 # print("Model:", MODEL)
 
-async def main():
-    pass
 
+def convert_mcp_tools(mcp_tools):
+    openai_tools = []
+
+    for tool in mcp_tools:
+
+        openai_tools.append({
+
+            "type" : "function",
+
+            "function" : {
+
+                "name" : tool.name,
+
+                "description" : tool.description,
+
+                "parameters" : tool.inputSchema
+
+            }
+        })
+
+    return openai_tools
+
+async def main():
+
+    server_params = StdioServerParameters(
+
+        command= "python",
+
+        args= ["sql_server.py"]
+
+    )
+
+    async with stdio_client(server_params) as (
+
+        read_stream,
+
+        write_stream
+
+    ):
+
+        async with ClientSession(
+
+            read_stream,
+
+            write_stream
+
+        ) as session:
+
+            await session.initialize()
+
+            tools = await session.list_tools()
+
+            openai_tools = convert_mcp_tools(tools.tools)
+
+            print("Type 'exit' whenever you want to quit...")
+
+            while True:
+
+                question = input("\nAsk something to your SQL Assistant:\n")
+
+                if question.lower() == "exit":
+
+                    break
+
+                messages = [
+                    {
+
+                        "role" : "user",
+
+                        "content" : question
+
+                    }
+                ]
+
+                while True:
+                    response = client.chat.completions.create(
+
+                        messages= messages,
+
+                        model= MODEL,
+
+                        tools= openai_tools,
+
+                        tool_choice= "auto"
+
+                    )
+
+                    assistant = response.choices[0].message
+
+                    if not assistant.tool_calls:
+
+                        print("Answer:\n\n")
+
+                        print(assistant.content)
+
+                        messages.append({
+                            "role" : "assistant",
+                            "content" : assistant.content
+                        })
+
+                        break
+
+                    messages.append(assistant)
+
+                    for tool_call in assistant.tool_calls:
+
+                        tool_name = tool_call.function.name
+
+                        arguments = json.loads(
+
+                            tool_call.function.arguments
+
+                        )
+
+                        print("\nCalling tool:", tool_name)
+
+                        result = await session.call_tool(
+
+                            name= tool_name,
+
+                            arguments= arguments
+
+                        )
+
+                        messages.append({
+
+                            "role" : "tool",
+
+                            "tool_call_id" : tool_call.id,
+
+                            "content" : str(result)
+
+                        })
 
 
 if __name__ == "__main__":
